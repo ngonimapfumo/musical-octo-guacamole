a582d39e7a7234e491abdf52b8c96244288068c6
Ngoni <ngonimapfumo@hotmail.com>
2025-03-22 17:09:15 +0200

    yo

--- Still/ViewController.swift
+++ Still/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var addAgent: UIButton!
+    
+    @IBOutlet weak var comms: UIButton!
+    
+    @IBOutlet weak var myAgents: UIButton!
+    
+    @IBOutlet weak var myProfile: UIButton!
+    
+    @IBOutlet weak var logout: UIButton!
+    
+
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Style buttons
+                styleButton(addAgent)
+                styleButton(comms)
+                styleButton(myAgents)
+                styleButton(myProfile)
+                styleButton(logout)
+                
+                // Highlight "MY COMMISSIONS" button
+              //  highlightButton(comms)
+    let imgView = UIImageView(image: UIImage(named: "sasai-logo-2"))
+        imgView.frame = CGRect(x: 100, y: 100, width: 200, height: 80)
+        view.addSubview(imgView)
     }
 
+    
+    func styleButton(_ button: UIButton) {
+            button.backgroundColor = UIColor.lightGray
+            button.layer.cornerRadius = 10
+        }
+        
+        func highlightButton(_ button: UIButton) {
+            button.layer.borderWidth = 2
+            button.layer.borderColor = UIColor.darkGray.cgColor
+        }
 
+       
+
+    @IBAction func actshon(_ sender: Any) {
+        
+        let alertController = UIAlertController(title: "Error", message: "message", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alertController, animated: true)
+        
+    }
 }
 
